10dd72dbce029dcf0b8b065f045ed9da2f8fc6e9
CesarNavarro24k <138552856+CesarNavarro24k@users.noreply.github.com>
2025-03-02 12:57:10 -0500

    Update contraseña.py

--- contraseña.py
+++ contraseña.py
@@ -7,3 +7,16 @@ contraseña_c = " "
 for i in range(contraseña):
     contraseña_c += random.choice(caracteres)
 print(contraseña_c)
+
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+    "GG" : "Good game"
+            }
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+if word in meme_dict.keys():
+    # ¿Qué debemos hacer si se encuentra la palabra?
+    print(meme_dict[word])
+else:
+    # ¿Qué hacer si no se encuentra la palabra?
+    print("No tenemos la palabra en el diccionario")
